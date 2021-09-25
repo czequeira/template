f2141ba9029075dcf91afffb5a6c40dd10ae4f79
Carlos Alberto Zequeira Sanchez <rafaelyrm@xetid.cu>
2021-09-24 21:41:25 -0400

    mas de bd

--- src/modules/db/db.module.ts
+++ src/modules/db/db.module.ts
@@ -14,6 +14,7 @@ import { services } from './services';
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
+      logging: false,
       models,
       synchronize: false,
     }),
--- src/providers/index.ts
+++ src/providers/index.ts
@@ -1,4 +1,6 @@
+export * from 'src/modules/db/services';
+import { services } from 'src/modules/db/services';
 export { HelloService } from './hello/hello.service';
 import { HelloService } from './hello/hello.service';
 
-export const providers = [HelloService];
+export const providers = [HelloService, ...services];
