5bb688c17de3380ba82087651c44a47af2eacbcb
Carlos Alberto Zequeira Sanchez <rafaelyrm@xetid.cu>
2021-09-30 20:49:53 -0400

    añadido el mecanismo de validacion

--- src/controllers/hello/hello.controller.ts
+++ src/controllers/hello/hello.controller.ts
@@ -1,14 +1,36 @@
-import { Controller, Get } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Get, Logger, Post } from '@nestjs/common';
+import { ApiProperty, ApiTags } from '@nestjs/swagger';
 import { HelloService } from 'src/providers';
+import { IsEmail, IsNotEmpty, IsNumber } from 'class-validator';
 
+class CreateUserDto {
+  @ApiProperty()
+  @IsEmail()
+  email: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  password: string;
+
+  @ApiProperty()
+  @IsNumber()
+  number: number;
+}
 @ApiTags('Hello')
 @Controller('hello')
 export class HelloController {
+  private readonly logger = new Logger(HelloController.name);
+
   constructor(private readonly helloService: HelloService) {}
 
   @Get()
   getHello(): string {
     return this.helloService.getHello();
+  }
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto): string {
+    this.logger.debug(JSON.stringify(createUserDto));
+    return 'Ok';
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
@@ -7,6 +8,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: new CustomLogger(),
   });
+
+  app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
     .setTitle(process.env.npm_package_name)
