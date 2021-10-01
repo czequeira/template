b725730fc189ac2b83a103d9c64408b0c8bc7bca
Carlos Alberto Zequeira Sanchez <rafaelyrm@xetid.cu>
2021-09-30 20:30:39 -0400

    modificado el logger middleware

--- src/middlewares/logger.middleware.ts
+++ src/middlewares/logger.middleware.ts
@@ -1,14 +1,24 @@
 import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
+import { ANSI_BLUE, ANSI_RESET } from 'src/utils';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   private readonly logger = new Logger(LoggerMiddleware.name);
 
-  use(req: any, __: any, next: () => void) {
-    const method = req.method;
-    const url = req.url;
-    const log = `${method}: ${url}`;
-    this.logger.log(log);
+  use(request: Request, response: Response, next: NextFunction) {
+    const { ip, method, path: url } = request;
+    const userAgent = request.get('user-agent') || '';
+
+    response.on('close', () => {
+      const { statusCode } = response;
+      const contentLength = response.get('content-length');
+
+      this.logger.log(
+        `${method} ${url} ${ANSI_BLUE}${statusCode}${ANSI_RESET} ${contentLength} - ${userAgent} ${ip}`,
+      );
+    });
+
     next();
   }
 }
